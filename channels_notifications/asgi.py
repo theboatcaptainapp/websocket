7a14d2b82dda0cbd6c185f2dda07d26f48ef805e
Tim <tjssnedden@gmai.com>
2020-10-02 11:39:27 +1300

    initial

---
+++ channels_notifications/asgi.py
@@ -0,0 +1,17 @@
+"""
+ASGI config for channels_notifications project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_notifications.settings')
+django.setup()
+application = get_default_application()
