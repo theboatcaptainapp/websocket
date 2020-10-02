11a72a993d98c5074021efb05ebf61dc2faea068
Tim <tjssnedden@gmai.com>
2020-10-02 18:57:55 +1300

    initial

--- channels_notifications/asgi.py
+++ channels_notifications/asgi.py
@@ -15,3 +15,5 @@ from channels.routing import get_default_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_notifications.settings')
 django.setup()
 application = get_default_application()
+
+
--- channels_notifications/settings.py
+++ channels_notifications/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notifications',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
