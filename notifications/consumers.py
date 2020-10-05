d229dceae2a1a2d0f912a91390dd695bde4ca01b
Tim <tjssnedden@gmai.com>
2020-10-06 12:25:38 +1300

    print recieve data

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -6,30 +6,30 @@ from pprint import pprint
 class NotificationConsumer(WebsocketConsumer):
     
     # Function to connect to the websocket
-    def connect(self):
+    async def connect(self):
        # Checking if the User is logged in
         if self.scope["user"].is_anonymous:
             # Reject the connection
-            self.close()
+            await self.close()
+
         else:
             # print(self.scope["user"])   # Can access logged in user details by using self.scope.user, Can only be used if AuthMiddlewareStack is used in the routing.py
             self.group_name = "test"  # Setting the group name as the pk of the user primary key as it is unique to each user. The group name is used to communicate with the user.
-            async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
-            self.accept()
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+            await self.accept()
 
     # Function to disconnet the Socket
-    def disconnect(self, close_code):
-        self.close()
+    async def disconnect(self, close_code):
+        await self.close()
         # pass
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        pprint(text_data_json)
         message = text_data_json['message']
         user = text_data_json['user']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             'test',
             {
                 'type': 'send_message',
@@ -38,6 +38,6 @@ class NotificationConsumer(WebsocketConsumer):
         )
 
     # Custom Notify Function which can be called from Views or api to send message to the frontend
-    def send_message(self, event):
-        self.send(text_data=json.dumps(event["text"]))
+    async def send_message(self, event):
+        await self.send(text_data=json.dumps(event["text"]))
 
