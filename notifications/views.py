783457dbc00bcea0698e1ee851d443a6d0c96806
Tim <tjssnedden@gmai.com>
2020-10-06 13:36:25 +1300

    print recieve data

--- notifications/views.py
+++ notifications/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
+from .models import Conversation, Message
+
 
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'notifications/home.html', {})
+    context = {}
+    conversation = Conversation.objects.get(name=test):
+    messages = Message.objects.filter(conversation=conversation)
+    context['messages'] = messages
+    return render(request, 'notifications/home.html', context)
 
 
 
